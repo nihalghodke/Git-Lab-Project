e96637bd0dc9ca294d29b7b8596f20ec4fba8536
NIhal Ghodke <ng.nihalghodke0908@gmail.com>
2026-09-11 14:13:56 +0530

    Add calculator project

---
+++ calculator.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter value of a=");
+    scanf("%d",&a);
+    printf("enter value of b=");
+    scanf("%d",&b);
+
+    printf(" addtion=%d\n",a+b);
+    printf("subtraction=%d\n",a-b);
+    return 0;
+}
